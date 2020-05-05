b19aee48503c2bde8f57c9fb069e3d7271498e68
Wang huahua <251154885@qq.com>
2020-05-05 15:57:50 +0800

    完成home静态组件拆分

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import TypeNav from "@/components/TypeNav";
+Vue.component("TypeNav", TypeNav);
 
 Vue.config.productionTip = false;
 
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    <h2>Home</h2>
+    <TypeNav />
+    <ListContainer />
+    <TodayRecommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <Floor />
+    <Brand />
   </div>
 </template>
 
 <script>
+import ListContainer from "./ListContainer/ListContainer";
+import TodayRecommend from "./TodayRecommend/TodayRecommend";
+import Rank from "./Rank/Rank";
+import Like from "./Like/Like";
+import Floor from "./Floor/Floor";
+import Brand from "./Brand/Brand";
 export default {
-  name: "",
+  name: "Home",
+  components: {
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  },
 };
 </script>
 
